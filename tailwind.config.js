14cbadcdc671e5d9fa12f2b6e3dc63a3a708cf82
Maclevison Silva <maclevison@gmail.com>
2021-02-14 15:27:16 +0000

    tailwind custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   theme: {
     extend: {},
+    colors: {
+      primary: "#4A2AF0",
+      secondary: "#0E0E0E",
+      light: "#FAFAFA"
+    },
+    fontFamily: {
+      sans: ["Anton", "sans-serif"],
+      mono: ["Roboto Mono", "monospace"]
+    },
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
